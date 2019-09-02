2c9700ec1ae4055d92186dce44ab692dd56658e1
RosaJio <54529547+RosaJio@users.noreply.github.com>
2019-09-02 17:36:05 +0700

    LAB5

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import  kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        var data  = intent.extras
+        var  newStudent = data?.getParcelable<Student>(  "stdData")
+        idText.text="Student ID : ${newStudent?.id}"
+        nameText.text="Stdent Name : ${newStudent?.name}"
+        ageText.text="Student Age : ${newStudent?.age}"
+    }
+    fun onClickClose(view:View){
+        finish()
+    }
+}
